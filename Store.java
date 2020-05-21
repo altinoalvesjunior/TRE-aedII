1a2a5276a3171786af2910585e767384242f3e95
Joey Clapton <joeyclapton42@gmail.com>
2020-05-20 21:18:04 -0300

    feat: cria classe para cadastro e impressão

---
+++ Store.java
@@ -0,0 +1,28 @@
+// Classe criada para gerenciar inserções
+public class Store {
+
+  /*
+  * Função para cadastrar Partido 
+  * @params {String} nome - nome do partido
+  * @params {String} sigla - sigla do partido
+  * @params {String} nomeArquivo - nome do arquivo a ser salvo
+  */
+  public void cadastrarPartido(String nome, String sigla) {
+    Files arquivo;
+    arquivo = new Files();
+
+    String[] dados = {nome, sigla};
+
+    arquivo.salvarArquivo("partidos.txt","Arquivo salvo com sucesso", dados);
+    
+    
+  }
+
+  public void imprimirPartidos() {
+    Files arquivo;
+    arquivo = new Files();
+
+    arquivo.lerDadosArquivo("partidos.txt");
+  }
+
+}
