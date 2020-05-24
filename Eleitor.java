a04706fcd5394f78aa8d2db30610d43c6eb68ab1
Altino Alves Júnior <altinoalvesjunior@gmail.com>
2020-05-24 18:24:15 -0300

    alteração atributo nome/ get/ set incorretos

--- Eleitor.java
+++ Eleitor.java
@@ -2,57 +2,57 @@
 public class Eleitor {
 
 	String nomeEleitor;
-	int tituloEleitora;
+	int tituloEleitor;
 	String municipioEleitoral;
 	int zonaEleitoral;
 	int secaoEleitoral;
 	
-	public Eleitor(String nomeEleitor, int tituloEleitora, String municipioEleitoral, int zonaEleitoral,
+	public Eleitor(String nomeEleitor, int tituloEleitor, String municipioEleitoral, int zonaEleitoral,
 			int secaoEleitoral) {
 		this.nomeEleitor = nomeEleitor;
-		this.tituloEleitora = tituloEleitora;
+		this.tituloEleitor = tituloEleitor;
 		this.municipioEleitoral = municipioEleitoral;
 		this.zonaEleitoral = zonaEleitoral;
 		this.secaoEleitoral = secaoEleitoral;
 	}
-
+	
 	public String getNomeEleitor() {
 		return nomeEleitor;
 	}
-
+	
 	public void setNomeEleitor(String nomeEleitor) {
 		this.nomeEleitor = nomeEleitor;
 	}
-
-	public int getTituloEleitora() {
-		return tituloEleitora;
+	
+	public int getTituloEleitor() {
+		return tituloEleitor;
 	}
-
-	public void setTituloEleitora(int tituloEleitora) {
-		this.tituloEleitora = tituloEleitora;
+	
+	public void setTituloEleitor(int tituloEleitor) {
+		this.tituloEleitor = tituloEleitor;
 	}
-
+	
 	public String getMunicipioEleitoral() {
 		return municipioEleitoral;
 	}
-
+	
 	public void setMunicipioEleitoral(String municipioEleitoral) {
 		this.municipioEleitoral = municipioEleitoral;
 	}
-
+	
 	public int getZonaEleitoral() {
 		return zonaEleitoral;
 	}
-
+	
 	public void setZonaEleitoral(int zonaEleitoral) {
 		this.zonaEleitoral = zonaEleitoral;
 	}
-
+	
 	public int getSecaoEleitoral() {
 		return secaoEleitoral;
 	}
-
+	
 	public void setSecaoEleitoral(int secaoEleitoral) {
 		this.secaoEleitoral = secaoEleitoral;
-	}
+	}	
 }
